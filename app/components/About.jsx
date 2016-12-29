81e2ed32f3a29791dd03640d307ed08c7ec0378a
aali2537 <alexander_li1@student.uml.edu>
2016-12-29 05:15:07 -0500

    Added information to the about page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -5,7 +5,8 @@ var About = (props) =>{
     <div className="columns medium-6 large-4 small-centered text-center">
       <h1 className="page-title">About</h1>
       <p>WeatherHere is an application that can grab weekly and current weather forecasts from anywhere in the world! WeatherHere is
-        <a href="https://darksky.net/poweredby/"> powered by DarkSky</a>.</p>
+         fully open sourced and can be found at <a className="active" href="https://github.com/aali2537/WeatherHere"> github</a>. Additionally WeatherHere
+          is powered by the Dark Sky Api which can be found <a className="active" href="https://darksky.net/poweredby/"> here</a>  .</p>
     </div>
   );
 }
